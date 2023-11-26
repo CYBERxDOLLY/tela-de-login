5d21507bf1362498a9e84b6e34b8918d7708eee7
CYBERxDOLLY <brayansoaresdeaguiar@gmail.com>
2023-11-26 20:51:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessageBox() {
+    let messageBox = document.getElementById('myMessageBox');
+    messageBox.style.display = 'block';
+}
+
+function hideMessageBox() {
+    let messageBox = document.getElementById('myMessageBox');
+    messageBox.style.display = 'none';
+}
