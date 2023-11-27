20f7cfe7cb58b4f61e81f430a252b8806f4567a3
CYBERxDOLLY <brayansoaresdeaguiar@gmail.com>
2023-11-27 19:29:58 -0300

    alerta de fechamento de guia

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function hideMessageBox() {
     let messageBox = document.getElementById('myMessageBox');
     messageBox.style.display = 'none';
 }
+
+//mensagem para a alertar o usuário que há alterações não salvas ao tentar fechar a página
+window.addEventListener("beforeunload", event => {
+    event.preventDefault()
+    event.returnValue = ''
+})
